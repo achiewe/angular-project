7f05df55b85843f39fd1538af4526cc8f4bf87f9
achiewe <mamukashviliachi706@gmail.com>
2024-02-02 13:40:48 +0400

    create the function notify which will alert and write hello world and this function start when click event on the button

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,4 +11,8 @@ import { RouterOutlet } from '@angular/router';
 export class AppComponent {
   title = 'angular-project';
   inputType = 'password';
+
+  notify() {
+    alert('hello world');
+  }
 }
